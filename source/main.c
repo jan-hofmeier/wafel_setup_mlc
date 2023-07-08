b1551e9eb403a298a45bb2092e48a081e8d72faa
Jan Hofmeier <jan.hofmeier@gmail.com>
2023-07-08 21:27:37 +0200

    use patch macros provided by stroopwafel

--- source/main.c
+++ source/main.c
@@ -5,33 +5,11 @@
 #include <wafel/dynamic.h>
 #include <wafel/ios_dynamic.h>
 #include <wafel/utils.h>
+#include <wafel/patch.h>
 #include <wafel/ios/svc.h>
 #include <wafel/ios/memory.h>
 
 #include "setup.h"
-
-
-#define _ASM_PATCH(_addr, _str, _copy_fn) { \
-    __asm__ volatile (           \
-    ".globl pre_" #_addr "\n"    \
-    ".globl post_" #_addr "\n"   \
-    "b post_" #_addr "\n"        \
-    "pre_" #_addr ":\n"          \
-    ".arm\n"                     \
-    _str "\n"                    \
-     ".arm\n"                    \
-    "post_" #_addr ":\n");       \
-    extern void pre_##_addr();   \
-    extern void post_##_addr();  \
-    _copy_fn((uintptr_t)_addr, (uintptr_t)pre_##_addr, (u32)post_##_addr - (u32)pre_##_addr); \
-}
-
-static void _patch_copy_k(uintptr_t dst, uintptr_t src, size_t sz)
-{
-    memcpy((void*)ios_elf_vaddr_to_paddr(dst), (void*)src, sz);
-}
-
-#define ASM_PATCH_K(_addr, _str) _ASM_PATCH(_addr, _str, _patch_copy_k)
 
 // This fn runs before everything else in kernel mode.
 // It should be used to do extremely early patches
