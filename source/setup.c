0679444a90fdad7b5303d07971d6e4bca29cde6a
Jan Hofmeier <jan.hofmeier@gmail.com>
2023-07-09 02:45:56 +0200

    logging

--- source/setup.c
+++ source/setup.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <wafel/utils.h>
 #include <wafel/services/fsa.h>
@@ -9,8 +10,31 @@
 #include "sci.h"
 #include "led.h"
 
+#define MAX_LOG_LINE_LENGHT 512
+#define CROSS_PROCESS_HEAP_ID 0xcaff
+
 char *folders_to_create[] = { "/vol/storage_mlc01/usr", "/vol/storage_mlc01/usr/boss", "/vol/storage_mlc01/usr/save", "/vol/storage_mlc01/usr/save/00050010",
                      "/vol/storage_mlc01/usr/packages", "/vol/storage_mlc01/usr/tmp", NULL };
+
+
+static int write_log(int fsaHandle, int logHandle, const char *operation, const char *path, int res){
+    if(!logHandle)
+        return -1;
+    void *dataBuffer = iosAllocAligned(CROSS_PROCESS_HEAP_ID, MAX_LOG_LINE_LENGHT, 0x40);
+    if(!dataBuffer){
+        debug_printf("Error allocating log buffer\n");
+    }
+    snprintf(dataBuffer, MAX_LOG_LINE_LENGHT, "%s;%s;-%08X\n", operation, path, res);
+    res = FSA_WriteFile(fsaHandle, dataBuffer, strnlen(dataBuffer, MAX_LOG_LINE_LENGHT), 1, logHandle, 0);
+    iosFree(CROSS_PROCESS_HEAP_ID, dataBuffer);
+    if(res == 1)
+        res = FSA_FlushFile(fsaHandle, logHandle);
+    if(res<0){
+      debug_printf("Error writing log: -%08X\n", -res);
+      return -1;
+    }
+    return 0;
+}
 
 void mount_sd(int fd, char* path)
 {
@@ -42,14 +66,14 @@ void wait_mlc_ready(int fd){
     debug_printf("MLC ready!\n");
 }
 
-int flush_mlc(int fd){
-    int ret = FSA_FlushVolume(fd, "/vol/storage_mlc01");
+int flush_mlc(int fsaHandle){
+    int ret = FSA_FlushVolume(fsaHandle, "/vol/storage_mlc01");
     debug_printf("Flush MLC returned %X\n", ret);
     return ret;
 }
 
-int flush_slc(int fd){
-    int ret = FSA_FlushVolume(fd, "/vol/system");
+int flush_slc(int fsaHandle){
+    int ret = FSA_FlushVolume(fsaHandle, "/vol/system");
     debug_printf("Flush SLC returned %X\n", ret);
     return ret;
 }
@@ -81,18 +105,22 @@ void update_error_state(int value, int level){
     }
 }
 
-void install_all_titles(int fd, char *directory){
+void install_all_titles(int fd, char *directory, int logHandle){
     int dir = 0;
     int ret = FSA_OpenDir(fd, directory, &dir);
+    write_log(fd,logHandle, "OpenDir", directory, ret);
     if(ret)
     {
+        update_error_state(1, 2);
         debug_printf("Dir %s open failed: %X Aborting...\n", directory, ret);
         return;
     }
 
     int mcp_handle = iosOpen("/dev/mcp", 0);
+    write_log(fd,logHandle, "OpenMCP", directory, ret);
     if(mcp_handle <= 0)
     {
+        update_error_state(1, 2);
         debug_printf("Failed to open MCP : -%08X\n", mcp_handle);
         FSA_CloseDir(fd, dir);
         return;
@@ -101,6 +129,7 @@ void install_all_titles(int fd, char *directory){
     directoryEntry_s *dir_entry = iosAlloc(0x00001, sizeof(directoryEntry_s));
     if(dir_entry == NULL)
     {
+        update_error_state(1, 2);
         debug_printf("Dir entry alloc failed, Aborting...\n");
         iosClose(mcp_handle);
         FSA_CloseDir(fd, dir);
@@ -111,6 +140,7 @@ void install_all_titles(int fd, char *directory){
     char *install_dir = iosAlloc(0x00001,0x100);
     if(install_dir == NULL)
     {
+        update_error_state(1, 2);
         debug_printf("Failed to allocate copy stringname!\n");
         iosFree(0x00001, dir_entry);
         iosClose(mcp_handle);
@@ -128,10 +158,12 @@ void install_all_titles(int fd, char *directory){
             // test if installable
             ret = MCP_InstallGetInfo(mcp_handle, install_dir);
             debug_printf("installinfo %s: %08x\n", dir_entry->name, ret);
-            update_error_state(ret, 1);
+            update_error_state(!ret, 1);
+            write_log(fd,logHandle, "InstallInfo", dir_entry->name, ret);
             if(!ret){
                 ret = install_title(mcp_handle, install_dir);
                 update_error_state(ret, 2);
+                write_log(fd,logHandle, "Install", dir_entry->name, ret);
             }
         }
     }
@@ -148,45 +180,61 @@ u32 setup_main(void* arg){
 
     debug_printf("START MLC SETUP\n");
 
-    int fd = -1;
+    int fsaHandle = -1;
     int i = 1;
-    while(fd < 0)
+    while(fsaHandle < 0)
     {
         usleep(1000);
-        fd = FSA_Open();
-        debug_printf("FSA open attempt %d %X\n", i++, fd);
+        fsaHandle = FSA_Open();
+        debug_printf("FSA open attempt %d %X\n", i++, fsaHandle);
     }
 
-    wait_mlc_ready(fd);
+    wait_mlc_ready(fsaHandle);
 
     SetNotificationLED(NOTIF_LED_BLUE_BLINKING);
 
-    int sys_quote_ret = FSA_MakeQuota(fd, "/vol/storage_mlc01/sys", 0, 3221225472);
-    debug_printf("MakeQuota /vol/storage_mlc01/sys -%X\n", -sys_quote_ret);
-    update_error_state(sys_quote_ret, 1);
+    int sys_quota_ret = FSA_MakeQuota(fsaHandle, "/vol/storage_mlc01/sys", 0, 3221225472);
+    debug_printf("MakeQuota /vol/storage_mlc01/sys -%X\n", -sys_quota_ret);
+    update_error_state(sys_quota_ret, 1);
 
     int folder_ret[sizeof(folders_to_create) / sizeof(folders_to_create[0])] = { 0 };
     for(i = 0; folders_to_create[i]; i++){
-        folder_ret[i] = FSA_MakeDir(fd, folders_to_create[i], 0);
+        folder_ret[i] = FSA_MakeDir(fsaHandle, folders_to_create[i], 0);
         debug_printf("Create %s -%X\n", folders_to_create[i] -folder_ret[i]);
         update_error_state(folder_ret[i], 1);
     }
 
-    int flush_ret = flush_mlc(fd);
+    int flush_ret = flush_mlc(fsaHandle);
     update_error_state(flush_ret, 2);
     
-    mount_sd(fd, "/vol/sdcard/");
-    install_all_titles(fd, "/vol/sdcard/wafel_install");
-    int ret = flush_mlc(fd);
+    mount_sd(fsaHandle, "/vol/sdcard/");
+
+    int logHandle = 0;
+    int ret = FSA_OpenFile(fsaHandle, "/vol/sdcard/wafel_setup_mlc.log", "w", &logHandle);
+    debug_printf("Open logfile -%X\n", -ret);
+    update_error_state(ret, 1);
+
+    // write out log entries from before the SD was mounted
+    write_log(fsaHandle, logHandle, "MakeQuota",  "/vol/storage_mlc01/sys", sys_quota_ret);
+    for(i = 0; folders_to_create[i]; i++){
+        write_log(fsaHandle, logHandle, "MakeDir", folders_to_create[i], folder_ret[i]);
+    }
+    write_log(fsaHandle, logHandle, "Flush", "MLC", flush_ret);
+
+    install_all_titles(fsaHandle, "/vol/sdcard/wafel_install", logHandle);
+    flush_ret = flush_mlc(fsaHandle);
     update_error_state(flush_ret, 2);
+    write_log(fsaHandle, logHandle, "Flush", "MLC", flush_ret);
 
-    int reset_ret = SCISetInitialLaunch(255);
-    debug_printf("Set InitalLaunch returned %X\n", reset_ret);
-    update_error_state(reset_ret, 2);
-    ret = flush_slc(fd);
+    ret = SCISetInitialLaunch(255);
+    debug_printf("Set InitalLaunch returned %X\n", ret);
     update_error_state(ret, 2);
+    write_log(fsaHandle, logHandle, "SetInitialLaunch", "255", ret);
+    ret = flush_slc(fsaHandle);
+    update_error_state(ret, 2);
+    write_log(fsaHandle, logHandle, "Flush", "SLC", ret);
 
-    iosClose(fd);
+    iosClose(fsaHandle);
 
     if(!error_state){
         SetNotificationLED(NOTIF_LED_BLUE);
